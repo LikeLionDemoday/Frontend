0ecb94872f74ab16a4b6e3ccf4f70925a993d298
root <root@GalaxyBookPro3>
2024-11-18 18:59:22 +0900

    Feat GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React from "react";
+import { useEffect } from "react";
+import { reset } from "styled-reset";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./fonts/Pretendard.css";
+import { createGlobalStyle } from "styled-components";
+
 import { DayCost } from "./Components/DayCost";
 import { DummyData } from "./Components/DummyData";
 
 function App() {
   return (
-    <div>
-      <DummyData/>
-    </div>
+    <>
+    <GlobalStyle />
+      <Router>
+        <Routes>
+          <DummyData/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  #root{
+    max-width: 390px;
+    height: var(--app-height, 100vh);
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: "Pretendard";
+    padding: 20px 20px 0;
+    background-color: #fff;
+    border: 2px solid #f4f4f4;
+  }
+`
