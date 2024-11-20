97731fdd67c810f42836fd2936287f423d0bbfb8
이제원 <lys28750@naver.com>
2024-11-20 13:43:20 +0900

    Feat merge중복 삭제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from "react";
-import { useEffect } from "react";
-import { reset } from "styled-reset";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./fonts/Pretendard.css";
-import { createGlobalStyle } from "styled-components";
+
 
 import React from "react";
 import { useEffect } from "react";
-import { reset } from "styled-reset";
+//import { reset } from "styled-reset";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./fonts/Pretendard.css";
+//import "./fonts/Pretendard.css";
 import { createGlobalStyle } from "styled-components";
 
 import { DayCost } from "./Components/DayCost";
 import { DummyData } from "./Components/DummyData";
 import { TravelDetail } from "./travelSpecific/TravelDetail";
-import { AnalysisExp } from "./expAdd/AnalysisExp";
 import { TravelDetailEdit } from "./travelSpecific/TravelDetailEdit";
 import { AnalysisExp } from "./expAdd/AnalysisExp";
-import { TravelDetailEdit } from "./travelSpecific/TravelDetailEdit";
+
 
 function App() {
   return (
@@ -32,34 +26,19 @@ function App() {
 }
 export default App;
 
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  #root{
-    max-width: 375px;
-    height: var(--app-height, 100vh);
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: "Pretendard";
-    padding: 20px 20px 0;
-    background-color: #fff;
-    border: 2px solid #f4f4f4;
-  }
-`
+// const GlobalStyle = createGlobalStyle`
+//   ${reset}
+//   #root{
+//     max-width: 375px;
+//     height: var(--app-height, 100vh);
+//     margin: 0 auto;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     font-family: "Pretendard";
+//     padding: 20px 20px 0;
+//     background-color: #fff;
+//     border: 2px solid #f4f4f4;
+//   }
+// `
 
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  #root{
-    max-width: 375px;
-    height: var(--app-height, 100vh);
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: "Pretendard";
-    padding: 20px 20px 0;
-    background-color: #fff;
-    border: 2px solid #f4f4f4;
-  }
-`
